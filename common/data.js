9811d472aae0da4e1ef063b4f59f1ce8dfc032ae
Yerko Palma <yerko.palma@usach.cl>
2016-10-21 01:54:07 +0000

    updated data

--- common/data.js
+++ common/data.js
@@ -1,24 +1,22 @@
-export default {
-  posts: [
-    {
-      title: 'post 1',
-      date: new Date(),
-      content: 'lorem ipsum...',
-      author: 'Yerko',
-      comments: []    
-    },
-    {
-      title: 'post 2',
-      date: new Date(),
-      content: 'lorem ipsum...',
-      author: 'Yerko',
-      comments: [
-        {
-          author: 'Salvador',
-          content: 'foo bar...',
-          date: new Date()
-        }
-      ]
-    }
-  ]
-}
+export const posts = [
+  {
+    title: 'post 1',
+    date: new Date(),
+    content: 'lorem ipsum...',
+    author: 'Yerko',
+    comments: []    
+  },
+  {
+    title: 'post 2',
+    date: new Date(),
+    content: 'lorem ipsum...',
+    author: 'Yerko',
+    comments: [
+      {
+        author: 'Salvador',
+        content: 'foo bar...',
+        date: new Date()
+      }
+    ]
+  }
+]
