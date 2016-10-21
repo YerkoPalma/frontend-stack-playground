38d57bfcd624e2376265b7c8f32d1d14e330b6a8
Yerko Palma <yerko.palma@usach.cl>
2016-10-21 17:37:26 +0000

    update data

--- common/data.js
+++ common/data.js
@@ -1,17 +1,21 @@
 export const posts = [
   {
-    title: 'post 1',
+    title: 'First blog post',
     date: new Date(),
-    content: 'lorem ipsum...',
-    excerpt: 'lorem ipsum...',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis enim vehicula est aliquam, blandit dignissim est ornare. Sed nec interdum erat. Nullam ultrices porta quam, quis laoreet dolor ultricies et. Aliquam porta nibh dolor, ac venenatis ligula placerat fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer auctor vitae ipsum at imperdiet. Sed pellentesque laoreet mi vel tincidunt. Nulla at ligula a tortor facilisis viverra rutrum eu mi. Nam sed mi lacus. Sed lorem mi, fringilla ornare egestas ut, posuere vitae nibh. Quisque in nisl eleifend augue posuere auctor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent nec leo bibendum, ultrices sem eu, hendrerit massa. Aliquam non ultricies tortor. Ut justo erat, ultrices sit amet ligula vitae, elementum venenatis mi.',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra porta tellus, sed vestibulum ipsum.',
     author: 'Yerko',
     comments: []    
   },
   {
-    title: 'post 2',
+    title: 'One more for the collection',
     date: new Date(),
-    content: 'lorem ipsum...',
-    excerpt: 'lorem ipsum...',
+    content: `Aliquam nulla neque, egestas elementum placerat facilisis, vulputate ornare mauris. Vestibulum molestie lacinia nibh, in cursus nisi. Quisque lorem neque, ornare ac nisl in, sollicitudin sodales lectus. Praesent maximus dictum facilisis. In blandit dictum cursus. Integer eu eleifend justo. Quisque tincidunt augue ante, sed euismod tellus facilisis et.
+
+Fusce ut sapien id nibh ornare pellentesque. Vestibulum tempus massa sollicitudin ligula dapibus, non blandit arcu tincidunt. Nulla vel urna vel mi faucibus ultrices sit amet sed arcu. Duis augue risus, porttitor a aliquet quis, molestie sit amet dui. Aenean gravida, lorem non rutrum porttitor, magna ante laoreet ex, eu viverra est velit ultricies massa. Proin commodo sem et quam maximus egestas. In et vehicula est. Proin tempor turpis at libero rutrum, in varius lectus dictum. Duis ut tincidunt risus, eu mattis libero. Nullam a elit at ligula pretium dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam auctor suscipit lobortis. Etiam sit amet pulvinar turpis, in semper ex.
+
+Fusce ut sapien id nibh ornare pellentesque. Vestibulum tempus massa sollicitudin ligula dapibus, non blandit arcu tincidunt. Nulla vel urna vel mi faucibus ultrices sit amet sed arcu. Duis augue risus, porttitor a aliquet quis, molestie sit amet dui. Aenean gravida, lorem non rutrum porttitor, magna ante laoreet ex, eu viverra est velit ultricies massa. Proin commodo sem et quam maximus egestas. In et vehicula est. Proin tempor turpis at libero rutrum, in varius lectus dictum. Duis ut tincidunt risus, eu mattis libero. Nullam a elit at ligula pretium dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam auctor suscipit lobortis. Etiam sit amet pulvinar turpis, in semper ex.`,
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum viverra porta tellus, sed vestibulum ipsum.',
     author: 'Yerko',
     comments: [
       {
